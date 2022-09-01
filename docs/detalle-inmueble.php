dfdb98de5f88516e7972da14145fcb0f7a651514
cesarmario <cesarmariosdelosrios@gmail.com>
2022-09-01 17:44:00 -0300

    purga

--- docs/detalle-inmueble.php
+++ docs/detalle-inmueble.php
@@ -382,24 +382,7 @@
                   </ul>
                 </div>
               </div>
-  
-  
             </div>
-  
-            <div class="col-lg-4 mb-5 mb-lg-0">
-              <h3 class="footer-heading mb-4">Seguinos en las Redes</h3>
-  
-                  <div>
-                    <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                    <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-                  </div>
-  
-              
-  
-            </div>
-            
           </div>
           <div class="row pt-5 mt-5 text-center">
             <div class="col-md-12">
